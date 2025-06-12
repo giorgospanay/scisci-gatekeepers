e5ec2ba90e539ea88bf0593eaf310deb35f84108
Georgios Panayiotou <giorgospanay@gmail.com>
2025-06-12 21:13:01 +0200

    add slurm sh

---
+++ src/oa-collab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -J oa_collab
+#SBATCH -p general
+#SBATCH -o filename_%j.txt
+#SBATCH -e filename_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gpanayio@iu.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=72:00:00
+#SBATCH --mem=32G
+#SBATCH -A openalex
+
+#Load any modules that your program needs
+module load python/3.12.4
+
+
+#Run your program
+srun python collab-layer.py
