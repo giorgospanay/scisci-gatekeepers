99a124be2383ca80a03369da4b251892c324ac35
Georgios Panayiotou <giorgospanay@gmail.com>
2025-06-16 16:12:22 +0200

    new edition

--- src/collab-layer.py
+++ src/collab-layer.py
@@ -14,26 +14,19 @@ obj_path=f"{out_workspace_path}/obj"
 # Data path
 tsv_file = f"{raw_workspace_path}/works_core+basic+authorship+ids+funding+concepts+references+mesh.tsv"
 
-author_to_node = {}
-node_id_counter = 0
-output_file = f"{obj_path}/coauthorship.edgelist"
+output_edges = f"{obj_path}/coauthorship.edgelist"
 
-with open(output_file, "w") as f_out:
-    chunksize = 10_000
-    for chunk in pd.read_csv(tsv_file, sep="\t", usecols=["authorships:author:id"], chunksize=chunksize, dtype=str):
-        for authors_str in chunk["authorships:author:id"].fillna(""):
-            authors = authors_str.split(",")
-            authors = [a.strip() for a in authors if a.strip()]
-            if len(authors) < 2:
-                continue
+with open(output_edges, "w") as f_out:
+	chunksize = 10_000
+	for chunk in pd.read_csv(tsv_file, sep="\t", usecols=["authorships:author:id"], chunksize=chunksize, dtype=str):
+		for authors_str in chunk["authorships:author:id"].fillna(""):
+			authors = authors_str.split(",")
+			authors = [a.strip() for a in authors if a.strip()]
+			if len(authors) < 2:
+				continue
 
-            node_ids = []
-            for author in authors:
-                if author not in author_to_node:
-                    author_to_node[author] = node_id_counter
-                    node_id_counter += 1
-                node_ids.append(author_to_node[author])
+			auth_set=set(authors)
 
-            # Write undirected edges (only once per pair)
-            for u, v in itertools.combinations(sorted(set(node_ids)), 2):
-                f_out.write(f"{u} {v}\n")
+			# Write undirected edges (only once per pair)
+			for u, v in itertools.combinations(auth_set,2):
+				f_out.write(f"{u} {v}\n")
