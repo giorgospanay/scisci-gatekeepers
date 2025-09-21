a9e9fb79ca951fec12a70555cd52ddcbdbbe7dc7
Georgios Panayiotou <giorgospanay@gmail.com>
2025-09-21 18:36:39 +0200

    updateslurm

--- src/filtered-layers-gpu.sh
+++ src/filtered-layers-gpu.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1              
 #SBATCH --cpus-per-task=8
-#SBATCH --time=4-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --mem=64G
 #SBATCH -A r00272
 
