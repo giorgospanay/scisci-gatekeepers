a32479086f9cde23c12f979a547b07d7a6a6a16f
Georgios Panayiotou <giorgospanay@gmail.com>
2025-10-22 19:48:19 +0200

    update

--- src/remake-sim-math.sh
+++ src/remake-sim-math.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-user=gpanayio@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=256G
 #SBATCH --time=2-00:00:00
 #SBATCH -A r00272
